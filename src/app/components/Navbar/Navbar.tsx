e527f5d1ad0985da0d5ed91fec313eec26719e65
nour-gamal <nmg181963@hotmail.com>
2025-07-09 23:54:03 +0300

    Refactor spacing mixins and update Navbar component for improved layout and padding

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -5,30 +5,32 @@ import search from "@assets/search.svg";
 import cart from "@assets/cart.svg";
 
 const Navbar = () => {
-  return (
-    <nav id="navbar" className="flex justify-around items-center">
-      <section className="navbar-brand">
-        <Link href='/'>
-            <Image src={logo} alt="mobilia logo" />
-        </Link>
-      </section>
-      <main id="navbar-links-desktop" className="flex grow">
-        <ul className="flex justify-around grow">
-          <li className="display-2 cursor-pointer">Home</li>
-          <li className="display-2 cursor-pointer">Shop</li>
-          <li className="display-2 cursor-pointer">About</li>
-          <li className="display-2 cursor-pointer">Contact</li>
-        </ul>
-      </main>
-      <section id="navbar-cart" className="flex grow justify-around">
-        <Link href="/search">
-             <Image src={search} alt="search" />
-         </Link>
-        <Link href="/cart">
-             <Image src={cart} alt="cart icon" />
-         </Link>
-      </section>
-    </nav>
-  );
-}
+	return (
+		<nav
+			id="navbar"
+			className="flex justify-between sm:justify-around items-center padding-x-lg">
+			<section className="navbar-brand">
+				<Link href="/">
+					<Image src={logo} alt="mobilia logo" />
+				</Link>
+			</section>
+			<main id="navbar-links-desktop" className="flex grow-1 hidden sm:block">
+				<ul className="flex justify-around grow">
+					<li className="display-2 cursor-pointer">Home</li>
+					<li className="display-2 cursor-pointer">Shop</li>
+					<li className="display-2 cursor-pointer">About</li>
+					<li className="display-2 cursor-pointer">Contact</li>
+				</ul>
+			</main>
+			<section id="navbar-cart" className="flex">
+				<Link href="/search">
+					<Image src={search} alt="search" className="margin-x-md" />
+				</Link>
+				<Link href="/cart">
+					<Image src={cart} alt="cart icon" className="margin-x-md" />
+				</Link>
+			</section>
+		</nav>
+	);
+};
 export default Navbar;
