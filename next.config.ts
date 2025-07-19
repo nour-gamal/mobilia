6ca6d82cfca9b62c4649fbc87a476c0c58cad1b1
nour-gamal <nmg181963@hotmail.com>
2025-07-19 22:07:51 +0300

    chore: remove webpack alias configuration from next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,18 +2,7 @@ import type { NextConfig } from "next";
 import path from "path";
 
 const nextConfig: NextConfig = {
-  // Add your config options here
-  webpack: (config) => {
-    config.resolve.alias = {
-      ...(config.resolve.alias || {}),
-      "@components": path.resolve(__dirname, "src/app/components"),
-      "@styles": path.resolve(__dirname, "src/app/styles"),
-      "@utils": path.resolve(__dirname, "src/app/lib/utils"),
-      "@assets": path.resolve(__dirname, "src/app/lib/assets"),
-      "@": path.resolve(__dirname),
-    };
-    return config;
-  },
+	// Add your config options here
 };
 
 export default nextConfig;
